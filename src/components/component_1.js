7b844e11e837d48ac375dd8495c32efdb8ce33c2
fromryan <johndoe@example.com>
2020-03-04 16:20:27 -0800

    first commit

--- src/components/component_1.js
+++ src/components/component_1.js
@@ -2,33 +2,14 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-
-class PersonCard extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            age: props.age
-        }
-    }
-    
-    
-    handleClick = () => {
-        this.setState({
-            age: this.state.age += 1
-        })
-    }
-
-    render() {
-        const {firstName, lastName, age, hairColor} = this.props
-        return (
-            <div class="mb-5">
-                <h1 class="display-5">{lastName}, {firstName}</h1>
-                <p>Age: {this.state.age}</p>
-                <p>Hair Color: {hairColor}</p>
-                <button type="button" class="btn btn-secondary" onClick={this.handleClick}>Birthday Button for {firstName} {lastName}</button>
-            </div>
-        )
-    }
+const PersonCard = (props) => {
+    return (
+        <div class="mb-5">
+        <h1 class="display-5">{props.lastName}, {props.firstName}</h1>
+        <p>Age: {props.age}</p>
+        <p>Hair Color: {props.hairColor}</p>
+    </div>
+    )
 }
 
 export default PersonCard;
